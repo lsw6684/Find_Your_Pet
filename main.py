7ba3dd12c0b6020711f5c439084d1d3e174cbd1a
lsw6684 <lsw6684@hanmail.net>
2021-05-25 10:02:31 +0900

    nlp

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from pymongo import MongoClient
 from twitter import extract_tweets
 from openAPI import extract_info
+from keys import s_MongoDB
+
+# URL로 호스트, 포트 지정
+cluster = "mongodb+srv://FYP:{}@cluster0.jzw1i.mongodb.net/test?retryWrites=true&w=majority".format(s_MongoDB)
+# collection 지정
+client = MongoClient(cluster)
+
+#print(client.list_database_names())
+
+db = client.test
+
+#print(db.list_collection_names())
+
+#print(client.list_database_names())
+
 print(extract_tweets())
 print("------------")
-print(extract_info())
+# print(extract_info()) 
--- twitter.py
+++ twitter.py
@@ -1,30 +1,39 @@
 import tweepy
 from keys import twitter
 
+# API Key.
+key = twitter[0]
+s_key = twitter[1]
+token = twitter[2]
+s_token = twitter[3]
+# 핸들러 생성, 개인정보 인증 요청
+auth = tweepy.OAuthHandler(key, s_key)
+# Access 요청
+auth.set_access_token(token, s_token)
+# twitter API 생성
+api = tweepy.API(auth)
+keyword = '안락사'
+tweets = api.search(keyword, count = 1) # 트윗 개수(RT는 밑 함수에서 제거)
+result = []
 def extract_tweets():
-    # API Key.
-    key = twitter[0]
-    s_key = twitter[1]
-    token = twitter[2]
-    s_token = twitter[3]
-    # 핸들러 생성, 개인정보 인증 요청
-    auth = tweepy.OAuthHandler(key, s_key)
-    # Access 요청
-    auth.set_access_token(token, s_token)
-    # twitter API 생성
-    api = tweepy.API(auth)
-    keyword = '안락사'
-    tweets = api.search(keyword, count = 30) # 트윗 개수(RT는 밑 함수에서 제거)
-
-    result = []
+    
     # RT와 원본 게시글의 형태가 너무 다르다. >> RT를 버리자.
     for num, tweet in enumerate(tweets):                # num 번호. RT포함하면 살릴거임.
         tmp = []
         if "RT" not in tweet.text:
-            tmp.append(tweet.created_at)                # 트윗 번호, 날짜 - 한국 시간 아닌 듯..
+            tmp.append(tweet.created_at)                # 날짜 - 한국 시간 아닌 듯..
             tmp.append(tweet.user.profile_image_url)    # 작성자 프로필 이미지 null
             tmp.append(tweet.user.name)                 # 닉네임
             tmp.append(tweet.user.screen_name)          # 트위터 @id
             tmp.append(tweet.text)                      # 트윗 내용
             result.append(tmp)
     return result
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import re
+
+X = []
+for i in tweets:
+    X.append(i.text)
+print(X)
