0e51fd34c2f6fceb51a7585be1fca8b95a36bd53
lsw6684 <lsw6684@hanmail.net>
2021-06-17 19:37:11 +0900

    Scheduler

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from twitter import extract_tweets
 from openAPI import extract_info
 from keys import s_MongoDB
 import schedule
+import time
 
 # URL로 호스트, 포트 지정
 cluster = 'mongodb+srv://FYP:{}@cluster0.jzw1i.mongodb.net/test?retryWrites=true&w=majority'.format(s_MongoDB)
 # collection 지정
 client = MongoClient(cluster)
 
-db = client.gettingStarted
+db = client.open_data
 
 infos = db.infos
 
-def store_infos():
+def store_infos(): 
     for i in extract_info():
         infos.insert_one(
             {
@@ -26,10 +27,11 @@ def store_infos():
                 "state" : i[6]
             }
         )
-def test():
-    print("test")
-schedule.every(1).second.do(test)
-
+# 00시 01분 : 전날 데이터 업데이트
+schedule.every().day.at("0:1").do(store_infos)
+while True:                 # 무한 루프로 스케줄 유지
+	schedule.run_pending() 
+	time.sleep(1) 
 
 #print(extract_tweets())
 #print("●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●")
